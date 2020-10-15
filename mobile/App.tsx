de7cdc48211ab4148ffc1a02bf5c9a94d14c7afc
AloisioFernandes <68351209+AloisioFernandes@users.noreply.github.com>
2020-10-15 14:40:04 -0300

    Mapa e marcador

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
+
+import mapMarker from './src/images/map-marker.png'
 
 export default function App() {
   return (
@@ -14,7 +16,25 @@ export default function App() {
           latitudeDelta: 0.008,
           longitudeDelta: 0.008
         }}
-      />
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8
+          }}
+          coordinate={{
+            latitude: -13.253305,
+            longitude: -43.408363
+          }}
+        >
+          <Callout tooltip onPress={() => { alert('eae')}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -27,5 +47,19 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height
+  },
+
+  calloutContainer: {
+    width: 168,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center'
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14
   }
 });
